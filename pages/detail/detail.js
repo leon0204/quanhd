fa6bf4e3333ee89d608189590390ca8eb8a08478
leon0204 <291838322@qq.com>
2018-12-06 18:02:58 +0800

    增加详情页
    增加详情页

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,8 +1,12 @@
-// pages/index/index.js
+// pages/detail/detail.js
 import {
   HTTP
 } from "../../utils/http.js"
 let http = new HTTP
+
+//获取应用实例
+var app = getApp();
+
 Page({
 
   /**
@@ -15,76 +19,83 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
     http.request({
       //获取test参数 
-      url: "priceList",
+      url: "priceDetail",
+      data:{
+        IDStr:"568240400765"
+      },
       success: (res) => {
 
-        for (var k in res.o_data) {
-          var arr = res.o_data[k].TicketPrice.match(/\d+(.\d+)?/g);
+        wx.setNavigationBarTitle({
+          title: res.Name//页面标题为路由参数
+        })
 
-          res.o_data[k].PriceAfter = res.o_data[k].Price - parseInt(arr[1])
-          if ((res.o_data[k].PriceAfter).toString().length >= 4) {
-            res.o_data[k].PriceAfter = (res.o_data[k].PriceAfter).toFixed(2)
+
+          var arr = res.TicketPrice.match(/\d+(.\d+)?/g);
+
+          res.PriceAfter = res.Price - parseInt(arr[1])
+          if ((res.PriceAfter).toString().length >= 4) {
+            res.PriceAfter = (res.PriceAfter).toFixed(2)
           }
-        }
 
 
         this.setData({
-          listData: this.data.listData.concat(res.o_data)
+          listData: this.data.listData.concat(res)
         })
-        // console.log(res.o_data)
+        console.log(res)
       }
     })
   },
 
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   }
 })
